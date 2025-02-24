568a428a9583b3e509fd3da41cbce7aaefa4134e
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-02-24 11:03:14 +0700

    updated test server

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('DATABASE_URL:', process.env.DATABASE_URL);
+console.log('DATABASE_URL:', process.env.DATABASE_URL_demo);
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL_demo,
 });
 
 export default pool;
