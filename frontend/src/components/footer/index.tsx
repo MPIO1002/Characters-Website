849415d12b3ebf973b7caaf29ba2e9cecfc97f28
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-02-24 10:11:13 +0700

    Initial commit

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const index = () => {
+  return (
+    <div className="bg-gray-950 p-4 text-white flex flex-col items-center justify-center h-50">
+      <p className="font-bold">Phát hành bởi Công ty Cổ phần GGO</p>
+      <p>Giấy phép cung cấp dịch vụ trò chơi điện tử G1 trên mạng số: 103/GP-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 16/3/2020</p>
+      <p>Quyết định phê duyệt nội dung kịch bản trò chơi điện tử G1 trên mạng số: 861/QĐ-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 21/05/2020</p>
+    </div>
+  )
+}
+
+export default index
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      customRed: '#A43627',
+    },
+  },
+};
+export const plugins = [];
