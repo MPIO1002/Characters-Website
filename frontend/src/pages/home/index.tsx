aadbd7381a6ea2e0d9ad1b271e7274a51fdee194
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-03-03 11:21:21 +0700

    update video

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import Heroes from '../../components/heroes-part'
+import Video from '../../components/video-banner/video'
 
 
 const index = () => {
     return (
         <div>
+            <Video />
             <Heroes />
         </div>
     )
