84a2f606b8d55dea3ff458d8f7469cb9c25216d7
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-06-16 12:38:54 +0700

    fix/pet: change text and color

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -2,7 +2,7 @@
 
 const index = () => {
   return (
-    <div className="bg-[#222222] p-4 text-white text-xs md:text-sm flex flex-col items-center justify-center h-50 text-center">
+    <div className="bg-[#1e1e1e] p-4 text-white text-xs md:text-sm flex flex-col items-center justify-center h-50 text-center">
       <p className="font-bold">Phát hành bởi Công ty Cổ phần GGO</p>
       <p>Giấy phép cung cấp dịch vụ trò chơi điện tử G1 trên mạng số: 103/GP-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 16/3/2020</p>
       <p>Quyết định phê duyệt nội dung kịch bản trò chơi điện tử G1 trên mạng số: 861/QĐ-BTTTT do Bộ Thông tin và Truyền thông cấp ngày 21/05/2020</p>
