a6d698c11e5f6866bd146204e1235d807f40a9ac
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-06-19 16:38:20 +0700

    feature/cache: build cache

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -4,6 +4,8 @@ import ArtifactPart from '../../components/artifact-part';
 import PetPart from '../../components/pet-part';
 import Video from '../../components/video-banner/video';
 import SideAd from '../../components/side-ad/side-ad';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAnglesUp } from '@fortawesome/free-solid-svg-icons';
 
 const Index = () => {
     const [activeTab, setActiveTab] = useState<'heroes' | 'artifact' | 'pet' >('heroes');
@@ -18,7 +20,7 @@ const Index = () => {
 
     // Hàm scroll lên đầu trang
     const handleScrollToTop = () => {
-        topRef.current?.scrollIntoView({ behavior: 'smooth' });
+        window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll lên đỉnh trang
     };
 
     // Hiện button khi cuộn tới đáy
@@ -51,9 +53,7 @@ const Index = () => {
                     className="fixed bottom-6 left-1/2 -translate-x-1/2 z-50 bg-white border border-gray-300 rounded-full p-2 shadow-lg hover:bg-gray-100 transition animate-bounce cursor-pointer"
                     aria-label="Lên đầu trang"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
-                    </svg>
+                    <FontAwesomeIcon icon={faAnglesUp} className="h-6 w-6 text-gray-700" />
                 </button>
             )}
         </div>
