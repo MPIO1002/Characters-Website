704308a8293bcbe4a88a23896a7e1e4cf540c022
Nguyễn Thị Thanh <147890041+MPIO1002@users.noreply.github.com>
2025-07-07 14:58:45 +0700

    feature/navbar: add link for button

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -1,7 +1,7 @@
 const index = () => {
     return (
         <div className="bg-[#222222] p-2 flex justify-between items-center top-0 left-0 w-full z-50">
-            <a href="/" className="flex-shrink-0">
+            <a href="https://mhgh.ggo.vn" className="flex-shrink-0">
                 <img src="logo.png" alt="logo" className="h-8 w-auto md:h-10 cursor-pointer" />
             </a>
             <div className="gap-2 md:gap-4 flex items-center flex-shrink-0">
@@ -10,7 +10,7 @@ const index = () => {
                         <img src="pay.PNG" alt="user" className="h-8 w-auto md:h-10 cursor-pointer" />
                     </button>
                 </a>
-                <a href="/">
+                <a href="https://mhgh.ggo.vn">
                     <button>
                         <img src="download.png" alt="cart" className="h-8 w-auto md:h-10 cursor-pointer" />
                     </button>
